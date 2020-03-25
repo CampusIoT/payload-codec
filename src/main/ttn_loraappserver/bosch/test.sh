8324b7f8a569d59e1cc96ea6568fafac1900a26f
Didier DONSEZ <didier.donsez@gmail.com>
2020-03-25 11:07:01 +0100

    loop on csvfile for test

--- src/main/ttn_loraappserver/bosch/test.sh
+++ src/main/ttn_loraappserver/bosch/test.sh
@@ -1,11 +1,10 @@
 #/bin/bash
 
-SKIP=1
-FILE="uplink.csv"
+FILE=$1
 CODEC="bosch_parking_codec.js"
 DATAENCODING="base64"
 while IFS=";" read -r fport data object
 do
     echo $fport $data $object
     node test.js $CODEC $DATAENCODING $fport $data $object
-done <  <(tail -n$((`cat "${FILE}" | wc -l` - SKIP)) "${FILE}")
+done <  <(tail -n +2 "${FILE}")
