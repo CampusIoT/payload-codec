b6ae2c04c65630001c181ce221f404a1c93fefe9
Didier DONSEZ <didier.donsez@gmail.com>
2020-03-25 10:36:54 +0100

    loop on csvfile for test

--- src/main/ttn_loraappserver/tekelek/test.js
+++ src/main/ttn_loraappserver/tekelek/test.js
@@ -1,8 +1,14 @@
 var argv = process.argv;
 var fs = require('fs');
 eval(fs.readFileSync(argv[2])+'');
-var port = parseInt(argv[3]);
-var payload = Buffer.from(argv[4], "hex");
+var dataEncoding = argv[3];
+var port = parseInt(argv[4]);
+var payload = Buffer.from(argv[5], dataEncoding);
+if(argv[6]) {
+    // For future use
+    var objectJSON = Buffer.from(argv[6]);
+}
+
 // console.log(payload.length, payload);
 var arr = Array.prototype.slice.call(payload, 0)
 
--- src/main/ttn_loraappserver/tekelek/test.sh
+++ src/main/ttn_loraappserver/tekelek/test.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
-node test.js ./tekelek_tek766_codec.js 16 "1000000000A30CAA00A20BAA00A20EAA00A620AA"  | jq .
-node test.js ./tekelek_tek766_codec.js 16 "1000000000A20BAA00A20EAA00A620AA00A30CAA"  | jq .
-node test.js ./tekelek_tek766_codec.js 48 "3000000201083700380064016806000D1753" | jq .
-node test.js ./tekelek_tek766_codec.js 16 "100000000033173A0033173A0000000000000000" | jq .
+SKIP=1
+FILE="uplink.csv"
+CODEC="tekelek_tek766_codec.js"
+DATAENCODING="hex"
+while IFS=";" read -r fport data object
+do
+    echo $fport $data $object
+    node test.js $CODEC $DATAENCODING $fport $data $object
+done <  <(tail -n$((`cat "${FILE}" | wc -l` - SKIP)) "${FILE}")
