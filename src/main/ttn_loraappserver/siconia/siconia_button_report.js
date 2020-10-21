b97e8f80ad193e4910800c580cbf791b5a49b019
Didier DONSEZ <didier.donsez@gmail.com>
2020-10-21 12:45:36 +0200

    add new port payload

--- src/main/ttn_loraappserver/siconia/siconia_button_report.js
+++ src/main/ttn_loraappserver/siconia/siconia_button_report.js
@@ -6,6 +6,10 @@
 var REPORT_PORT=1;
 var BUTTON_PORT=7;
 
+var REPORT_P_PORT=2;
+var BUTTON_P_PORT=8;
+var MOTION_P_PORT=10;
+
 function readInt16LE (buf, offset) {
   offset = offset >>> 0
   var val = buf[offset] | (buf[offset + 1] << 8);
@@ -20,11 +24,24 @@ function Decode(fPort, bytes) {
         temperature: bytes[0], // in C°
         humidity: bytes[1], // in %
         pressure: readInt16LE(bytes,2), // in hPa
-        battery_level: bytes[4], // in %
+        battery_level: bytes[4] // in %
       };
     } else {
       {error: "BAD_SIZE"};
     }
+  } else if((fPort===BUTTON_P_PORT || fPort===MOTION_P_PORT || fPort===REPORT_P_PORT)){
+    if(bytes.length === 6) {
+          return {
+            button: (fPort===BUTTON_P_PORT),
+           	motion: (fPort===MOTION_P_PORT),
+            battery_level: bytes[0], // in %
+            humidity:  bytes[1], // in %
+            temperature: readInt16LE(bytes,2)/10.0, // in C°
+            pressure: readInt16LE(bytes,4) // in hPa
+          };
+      } else {
+        {error: "BAD_SIZE"};
+      }
   } else {
 	   return {error: "UNKNOWN_PORT"};
   }
