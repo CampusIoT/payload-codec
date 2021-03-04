dc2b58c9e6a55019c83f576b593a0fb7cbecac5b
Didier Donsez <didier.donsez@gmail.com>
2021-03-04 09:42:38 +0100

    add decodeFloat32

--- src/main/ttn_loraappserver/buf.js
+++ src/main/ttn_loraappserver/buf.js
@@ -86,6 +86,27 @@ function readUInt8 (buf, offset) {
   return (buf[offset]);
 }
 
+
+/*
+*  Usage: decodeFloat32(readUInt32BE (buf, offset))
+*/ 
+function decodeFloat32(bytes) {
+    var sign = (bytes & 0x80000000) ? -1 : 1;
+    var exponent = ((bytes >> 23) & 0xFF) - 127;
+    var significand = (bytes & ~(-1 << 23));
+
+    if (exponent == 128)
+        return sign * ((significand) ? Number.NaN : Number.POSITIVE_INFINITY);
+
+    if (exponent == -127) {
+        if (significand == 0) return sign * 0.0;
+        exponent = -126;
+        significand /= (1 << 22);
+    } else significand = (significand | (1 << 23)) / (1 << 23);
+
+    return sign * significand * Math.pow(2, exponent);
+}
+
 /*
 
 // Decode the latitude returned by the Semtech LoRaMote.
