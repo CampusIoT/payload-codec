6f86723a6227945dbe73a48cd0ae92e28fdac63c
Didier DONSEZ <didier.donsez@gmail.com>
2021-06-10 14:45:52 +0200

    rename

--- src/main/javascript/nexelec/nexelec_origin_decoder.js
+++ src/main/javascript/nexelec/nexelec_origin_decoder.js
@@ -1,4 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////////
 // NEXELEC
 // THIS SOFTWARE IS PROVIDED BY NEXELEC ``AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -598,4 +597,22 @@ function Decoder(bytes) {
 // The function must return an object, e.g. {"temperature": 22.5}
 function Decode(fPort, bytes, variables) {
     return Decoder(bytes);
+}
+
+
+// Decode decodes an array of bytes of an uplink into an object.
+//  - fPort contains the LoRaWAN fPort number
+//  - bytes is an array of bytes, e.g. [225, 230, 255, 0]
+//  - variables contains the device variables e.g. {"calibration": "3.5"} (both the key / value are of type string)
+// The function must return an object, e.g. {"temperature": 22.5}
+function decodeUL(fPort, bytes, variables) {
+    return Decoder(bytes);
+}
+
+function decodeDL(fPort, bytes, variables) {
+    return null;
+}
+
+function encodeDL(obj, variables) {
+    return null;
 }
