540423801241dc610048cb5096fbaf4098a66fd4
Didier DONSEZ <didier.donsez@gmail.com>
2021-06-30 07:33:00 +0200

    improve nke watteco codec

--- src/main/javascript/nke_watteco/decodeZCL_svn5597.js
+++ src/main/javascript/nke_watteco/decodeZCL_svn5597.js
@@ -670,3 +670,13 @@ function Decoder(bytes, port) {
   return decoded;
 }
 
+
+
+// Decode decodes an array of bytes into an object.
+//  - fPort contains the LoRaWAN fPort number
+//  - bytes is an array of bytes, e.g. [225, 230, 255, 0]
+//  - variables contains the device variables e.g. {"calibration": "3.5"} (both the key / value are of type string)
+// The function must return an object, e.g. {"temperature": 22.5}
+function Decode(fPort, bytes, variables) {
+  return Decoder(bytes, fPort);
+}
